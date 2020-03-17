2569e938d271dbe6b68a84f62bffa3569a4c97b1
smokash <smokash@myz.net>
2020-03-17 18:51:39 +0800

    1

--- ip.php
+++ ip.php
@@ -1,2 +1,6 @@
 <?php
-echo $_SERVER["REMOTE_ADDR"];
+if (isset($_GET["ip"]) && filter_var($_GET["ip"], FILTER_VALIDATE_IP)) {
+    echo file_get_contents("http://ip-api.com/json/" .$_GET["ip"]. "?lang=zh-CN");
+} else {
+    echo $_SERVER["REMOTE_ADDR"];
+}
