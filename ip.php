a1a2b8ea5dccc049c4489f1f358c59e4c16bf0cd
smokash <you@example.com>
2020-03-31 22:51:37 +0800

    1

--- ip.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (isset($_GET["ip"]) && filter_var($_GET["ip"], FILTER_VALIDATE_IP)) {
-    echo file_get_contents("http://ip-api.com/json/" .$_GET["ip"]. "?lang=zh-CN");
-} else {
-    echo $_SERVER["REMOTE_ADDR"];
-}
